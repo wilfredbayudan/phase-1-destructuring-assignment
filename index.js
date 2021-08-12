7dee976807193ee420572ac9a767376f648f5aa0
Wilfred <wilfredbayudan@gmail.com>
2021-08-12 01:27:37 -1000

    Completed assignment

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const muppet = {
   partner: 'Kermit'
 };
 
+const {muppetName, color, song, job, partner} = muppet;
+
 const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
@@ -25,6 +27,15 @@ const nestedMuppet = {
   nestedPartner: 'Miss Piggy'
 };
 
+const {song2, song4} = nestedMuppet.album.theMuppetMovie;
+const {nestedJob, nestedPartner} = nestedMuppet;
+
+const [moo,neigh,baa,oink,cluck] = farmAnimals.split(" ");
+const [bessie,,dolly,babe,little] = farmAnimals.split(" ");
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [blackAndWhite,,black,pink] = farmAnimals.split(" ");
+const [r, o, y, g, b,, v] = colors;
+const [,,,,,indg] = colors;
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
